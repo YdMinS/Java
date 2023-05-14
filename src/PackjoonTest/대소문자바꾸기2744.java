61b9b317efea96326b12bdd0038375cb7ab5365b
ydmins <dr.ydmins@gmail.com>
2023-05-14 22:44:54 +0900

    2744
    대소문자 바꾸기 테스트문제 풀이

---
+++ src/PackjoonTest/대소문자바꾸기2744.java
@@ -0,0 +1,16 @@
+package PackjoonTest;
+
+import java.util.Scanner;
+
+public class 대소문자바꾸기2744 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        for(int i=0; i<str.length(); i++){
+            if('A' <= str.charAt(i) && str.charAt(i)<= 'Z'){
+                System.out.print((char)('a'+str.charAt(i)-'A'));
+            } else
+                System.out.print((char)('A'+str.charAt(i)-'a'));
+        }
+    }
+}
