234467b402845ab083d26989bb857e90861afda4
ydmins <dr.ydmins@gmail.com>
2023-05-26 12:18:20 +0900

    11727
    2xN 타일링 문제풀이

---
+++ src/PackjoonTest/Twoxn타일링11727.java
@@ -0,0 +1,24 @@
+package PackjoonTest;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Twoxn타일링11727 {
+    private static long[] s = new long[1002];
+    private static void count(int n){
+        if(s[n]==-1)
+            s[n] = s[n-1]%10007 + s[n-2]%10007;
+    }
+    public static void main(String[] args) {
+        Arrays.fill(s, -1);
+        s[1] = 1;
+        s[2] = 2;
+        int n = new Scanner(System.in).nextInt();
+        if(n>2){
+            for(int i=3; i<=n+1; i++){
+                count(i);
+            }
+        }
+        System.out.println(s[n]%10007);
+    }
+}
