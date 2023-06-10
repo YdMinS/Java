3cf4a0285bbfc372d0820a89ddf9de4155518cc6
ydmins <dr.ydmins@gmail.com>
2023-06-10 17:55:55 +0900

    1008
    수학 문제풀이

---
+++ src/PackjoonTest/수학1008.java
@@ -0,0 +1,12 @@
+package PackjoonTest;
+
+import java.util.Scanner;
+
+public class 수학1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        System.out.println(N/(double)M);
+    }
+}
