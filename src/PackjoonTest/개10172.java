b2904598089d9377a5650df9efe64de228c5d1cd
ydmins <dr.ydmins@gmail.com>
2023-06-10 19:58:18 +0900

    10172
    개 문제풀이

---
+++ src/PackjoonTest/개10172.java
@@ -0,0 +1,11 @@
+package PackjoonTest;
+
+public class 개10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
