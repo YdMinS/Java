51648d9bc9c1022222b209bb3369eba9dc4142dc
ydmins <dr.ydmins@gmail.com>
2023-06-10 19:51:10 +0900

    10699
    오늘 날짜 문제풀이

---
+++ src/PackjoonTest/오늘날짜10699.java
@@ -0,0 +1,9 @@
+package PackjoonTest;
+
+import java.time.LocalDate;
+
+public class 오늘날짜10699 {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+    }
+}
