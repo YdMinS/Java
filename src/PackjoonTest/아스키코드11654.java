2087b652611b29776161394eb3a9a7c84d2a9f84
ydmins <dr.ydmins@gmail.com>
2023-06-26 16:03:25 +0900

    11654
    아스키코드 문제풀이

---
+++ src/PackjoonTest/아스키코드11654.java
@@ -0,0 +1,11 @@
+package PackjoonTest;
+
+import java.util.Scanner;
+
+public class 아스키코드11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println((int)str.charAt(0));
+    }
+}
